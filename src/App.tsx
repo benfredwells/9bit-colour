5dde70faf7afe5740fa26056a7d74fb1e97a5cdf
benfred <benfred@mjaw.com>
2024-05-09 15:35:14 +1000

    Remove links and add extra button

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [green, setGreen] = useState(8)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>9 bit colours</h1>
+      <h2>An explorer</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setGreen((green) => green + 1)}>
+          Up
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        Green: {green}
+        <button onClick={() => setGreen((green) => green - 1)}>
+          Down
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
