43c72c79c0af8950490332978fddabaf348a4fc4
benfred <benfred@mjaw.com>
2024-05-22 13:51:39 +1000

    Add row for control colour, rejig colour specs

--- src/components/explorer.tsx
+++ src/components/explorer.tsx
@@ -6,42 +6,45 @@ import ColourSquare from "./colourSquare";
 function Explorer() {
   const [green, setGreen] = useState(0);
 
-  const redValues: Array<number> = Array.from(Array(8).keys());
-  const blueValues: Array<number> = Array.from(Array(8).keys());
+  const colourValues: Array<number> = Array.from(Array(8).keys());
 
-  const colourComponentForValue = (value: number) => {
+  const from3bitTo8 = (value: number): number => {
     let num = value << 5;
     if (num != 0) {
       num |= 0b00011111;
     }
-    let result = num.toString(16);
-    if (result.length == 1) {
-      result = "0" + result;
-    }
+    return num;
+  };
+
+  const colourForGridValues = (red: number, blue: number) => {
+    const result =
+      "rgb(" +
+      from3bitTo8(red) +
+      "," +
+      from3bitTo8(green) +
+      "," +
+      from3bitTo8(blue) +
+      ")";
+    console.log(result);
     return result;
   };
 
-  const colourForValues = (red: number, blue: number) => {
-    const result =
-      "#" +
-      colourComponentForValue(red) +
-      colourComponentForValue(green) +
-      colourComponentForValue(blue);
-    console.log(result);
+  const colourForControlValue = (value: number) => {
+    const result = "rgb(0, " + from3bitTo8(value) + ", 0 )";
     return result;
   };
 
   return (
     <>
       <div className="grid">
-        {redValues.map((red) => {
+        {colourValues.map((red) => {
           return (
             <div key={red} className="row">
-              {blueValues.map((blue) => {
+              {colourValues.map((blue) => {
                 return (
                   <ColourSquare
                     key={blue}
-                    colour={colourForValues(red, blue)}
+                    colour={colourForGridValues(red, blue)}
                   />
                 );
               })}
@@ -51,11 +54,16 @@ function Explorer() {
       </div>
       <h1>9 bit colours</h1>
       <h2>An explorer</h2>
-      <div className="card">
-        <button onClick={() => setGreen((green) => green - 1)}>Down</button>
-        Green: {green}
-        <button onClick={() => setGreen((green) => green + 1)}>Up</button>
+      <div className="row">
+        {colourValues.map((green) => {
+          return (
+            <ColourSquare key={green} colour={colourForControlValue(green)} />
+          );
+        })}
       </div>
+      <button onClick={() => setGreen((green) => green - 1)}>Down</button>
+      Green: {green}
+      <button onClick={() => setGreen((green) => green + 1)}>Up</button>
     </>
   );
 }
